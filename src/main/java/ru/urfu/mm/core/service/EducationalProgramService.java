340ad36495bdd3968a9f37832154f1420e223427
lev <lkorasik83@gmail.com>
2024-02-23 16:03:35 +0500

    Реализовать создание образовательных программ (#20)
    https://github.com/lkorasik/core/issues/19

--- src/main/java/ru/urfu/mm/core/service/EducationalProgramService.java
+++ src/main/java/ru/urfu/mm/core/service/EducationalProgramService.java
@@ -5,18 +5,20 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.urfu.mm.core.dto.CourseDTO;
-import ru.urfu.mm.core.dto.FullEducationalProgramDTO;
-import ru.urfu.mm.core.dto.FullSemesterDTO;
+import ru.urfu.mm.core.dto.*;
 import ru.urfu.mm.core.entity.EducationalProgram;
 import ru.urfu.mm.core.entity.EducationalProgramToCoursesWithSemesters;
 import ru.urfu.mm.core.entity.Semester;
+import ru.urfu.mm.core.entity.SpecialCourse;
 import ru.urfu.mm.core.repository.EducationalProgramRepository;
 import ru.urfu.mm.core.repository.EducationalProgramToCoursesWithSemestersRepository;
 import ru.urfu.mm.core.repository.SemesterRepository;
 import ru.urfu.mm.core.repository.SpecialCourseRepository;
 
+import java.time.Instant;
+import java.time.Year;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class EducationalProgramService {
@@ -118,5 +120,155 @@ public class EducationalProgramService {
                 credits,
                 finalSemesters
         );
+    }
+
+    public void createEducationalProgram(CreateEducationalProgramDTO createEducationalProgramDTO) throws JsonProcessingException {
+        createNecessarySemesters(createEducationalProgramDTO.getSemesters());
+        var semestersIds = getSemesters();
+
+        // Add module
+        // Add course
+
+        //var requiredCoursesIds = educationalProgramRequest.Semesters.Select(x => x.RequiredCourses).ToList();
+
+        var pairs = new ArrayList<List<UUID>>();
+        for(var semester : createEducationalProgramDTO.getSemesters()) {
+            var requiredCoursesIds = semester.getRequiredCourses();
+
+            var modulesIds = specialCourseRepository
+                    .findAllById(requiredCoursesIds)
+                    .stream()
+                    .map(x -> x.getEducationalModule().getId())
+                    .distinct()
+                    .toList();
+            var allCourses = modulesIds
+                    .stream()
+                    .map(this::getEducationalModuleCourses)
+                    .flatMap(Collection::stream)
+                    .toList();
+
+            // Надо выяснить какие модули мы испольщзуем
+            // Составить список всех курсов, которые есть в используемых модулях
+
+            var semesterIndex = createEducationalProgramDTO
+                    .getSemesters()
+                    .indexOf(semester);
+            requiredCoursesIds
+                    .forEach(x -> pairs.add(List.of(semestersIds.get(semesterIndex), x)));
+        }
+
+        // Программа
+        var programId = createEducationalProgram(semestersIds, createEducationalProgramDTO);
+        var models = pairs
+                .stream()
+                .map(pair -> {
+                    var semesterId = pair.get(0);
+                    var courseId = pair.get(1);
+
+                    var educationalProgram = educationalProgramRepository.findById(programId).get();
+                    var semester = semesterRepository.findById(semesterId).get();
+                    var course = specialCourseRepository.findById(courseId).get();
+
+                    return new EducationalProgramToCoursesWithSemesters(
+                            educationalProgram,
+                            semester,
+                            course,
+                            true
+                    );
+                })
+                .toList();
+        models.forEach(x -> educationalProgramToCoursesWithSemestersRepository.save(x));
+
+        // Добавить (программа, курс, семестр) в бд
+    }
+
+    public UUID createEducationalProgram(List<UUID> semestersIds, CreateEducationalProgramDTO educationalProgramRequest) throws JsonProcessingException {
+        Map<UUID, Integer> credits = new HashMap<>();
+        var length = Math.min(semestersIds.size(), educationalProgramRequest.getRecomendedCredits().size());
+        for (int i = 0; i < length; i++) {
+            credits.put(semestersIds.get(i), educationalProgramRequest.getRecomendedCredits().get(i));
+        }
+
+        var serializedCredits = serializer.writeValueAsString(credits);
+        var program = new EducationalProgram(
+                educationalProgramRequest.title,
+                educationalProgramRequest.title,
+                serializedCredits
+        );
+
+        educationalProgramRepository.save(program);
+
+        return program.getId();
+    }
+
+    public List<SpecialCourseDTO> getEducationalModuleCourses(UUID educationalModuleId) {
+        var courses = specialCourseRepository
+                .findAll()
+                .stream()
+                .filter(x -> educationalModuleId.equals(x.getEducationalModule().getId()))
+                .toList();
+
+        return courses
+                .stream()
+                .map(ModelConverterHelper::toDomain)
+                .toList();
+    }
+
+    /**
+     * Получить список актуальных семестров
+     */
+    private List<UUID> getSemesters() {
+        var years = List.of(
+                Year.now().getValue(),
+                Year.now().getValue() + 1,
+                Year.now().getValue() + 1,
+                Year.now().getValue() + 2
+        );
+        var semestersNumbers = List.of(1, 2, 3, 4);
+        return semesterRepository
+                .findAll()
+                .stream()
+                .filter(x -> years.contains(x.getYear()))
+                .map(Semester::getId)
+                .toList();
+    }
+
+    /**
+     * Добавить недостающие семестры
+     */
+    private void createNecessarySemesters(List<CreateSemesterDTO> semesters) {
+        var years = List.of(
+                Year.now().getValue(),
+                Year.now().getValue() + 1,
+                Year.now().getValue() + 1,
+                Year.now().getValue() + 2
+        );
+        var semestersNumbers = List.of(1, 2, 3, 4);
+
+        // Получаем список семестров, которые уже занесены в систему
+        var existingSemesters = semesterRepository
+                .findAll()
+                .stream()
+                .filter(x -> years.contains(x.getYear()) && semestersNumbers.contains(x.getSemesterNumber()))
+                .toList();
+
+        // Выичсляем какие семестры необходимо добавить в систему
+        var toAdd = new ArrayList<Semester>();
+        for(int i = 0; i < semesters.size(); i++) {
+            var item = new Semester(years.get(i), i + 1);
+            toAdd.add(item);
+        }
+        var toAdd2 = toAdd
+                .stream()
+                .filter(x -> !existingSemesters
+                        .stream()
+                        .map(y -> List.of(y.getYear(), y.getSemesterNumber()))
+                        .toList()
+                        .contains(List.of(x.getYear(), x.getSemesterNumber()))
+                )
+                .toList();
+
+        // Добавляем недостающие семестры
+        toAdd2.forEach(x -> semesterRepository.save(x));
     }
 }
