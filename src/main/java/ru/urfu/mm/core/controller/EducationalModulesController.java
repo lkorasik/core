0907cd675500a8d9c5aa6d75def6c888b7f3f70f
lev <lkorasik83@gmail.com>
2024-02-23 16:20:27 +0500

    Реализовать добавление модулей (#22)
    https://github.com/lkorasik/core/issues/21

--- src/main/java/ru/urfu/mm/core/controller/EducationalModulesController.java
+++ src/main/java/ru/urfu/mm/core/controller/EducationalModulesController.java
@@ -2,6 +2,7 @@ package ru.urfu.mm.core.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ru.urfu.mm.core.dto.CreateModuleDTO;
 import ru.urfu.mm.core.dto.GetModulesDTO;
 import ru.urfu.mm.core.entity.EducationalModule;
 import ru.urfu.mm.core.service.EducationalModulesService;
@@ -22,5 +23,12 @@ public class EducationalModulesController {
     @PostMapping
     public List<EducationalModule> getModulesById(@RequestBody GetModulesDTO getModulesDTO) {
         return educationalModulesService.getModulesByIds(getModulesDTO.getModulesIds());
+    }
+
+    @PostMapping("/create")
+    public void createModule(@RequestBody CreateModuleDTO createModuleDTO) {
+        educationalModulesService.createModuleWithCourses(
+                createModuleDTO.getEducationalModuleName(),
+                createModuleDTO.getSpecialCoursesIds());
     }
 }
