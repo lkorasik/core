05dd43f190e2991583b22cac6cc7e6b399ea55c4
lkorasik83 <lkorasik83@gmail.com>
2024-03-19 21:38:55 +0500

    DocumentController

--- app/src/main/java/ru/urfu/mm/controller/document/DocumentController.java
+++ app/src/main/java/ru/urfu/mm/controller/document/DocumentController.java
@@ -1,4 +1,4 @@
-package ru.urfu.mm.controller;
+package ru.urfu.mm.controller.document;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
