cf3abcb71236256e66a4b3d0df049ad70c266f9a
lev <lkorasik83@gmail.com>
2024-04-03 19:33:45 +0500

    Добавил различные конфигурации

--- app/src/main/java/ru/urfu/mm/configuration/UseCaseConfiguration.java
+++ app/src/main/java/ru/urfu/mm/configuration/UseCaseConfiguration.java
@@ -2,6 +2,7 @@ package ru.urfu.mm.configuration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jersey.JerseyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
