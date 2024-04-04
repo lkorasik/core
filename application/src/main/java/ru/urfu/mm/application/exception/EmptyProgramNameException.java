186e02138bdba7c461e1178a8765770d75785a57
lev <lkorasik83@gmail.com>
2024-04-04 23:14:15 +0500

    Rename domain

--- application/src/main/java/ru/urfu/mm/application/exception/EmptyProgramNameException.java
+++
@@ -1,7 +0,0 @@
-package ru.urfu.mm.application.exception;
-
-public class EmptyProgramNameException extends RuntimeException {
-    public EmptyProgramNameException() {
-        super("The program name cannot be empty.");
-    }
-}
--- application/src/main/java/ru/urfu/mm/application/gateway/ProgramGateway.java
+++
@@ -1,7 +0,0 @@
-package ru.urfu.mm.application.gateway;
-
-import ru.urfu.mm.domain.Program;
-
-public interface ProgramGateway {
-    void saveProgram(Program program);
-}
