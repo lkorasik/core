094f27d6f9568d507c98c4034efedb3038295724
lev <lkorasik83@gmail.com>
2024-04-21 16:02:34 +0500

    Рефакторинг получения прогармм (#64)

---
+++ app/src/main/java/ru/urfu/mm/service/mapper/ProgramToDomainMapper.java
@@ -0,0 +1,18 @@
+package ru.urfu.mm.service.mapper;
+
+import org.springframework.stereotype.Component;
+import ru.urfu.mm.domain.Program;
+import ru.urfu.mm.entity.EducationalProgram;
+
+@Component
+public class ProgramToDomainMapper implements Mapper<EducationalProgram, Program> {
+    @Override
+    public Program map(EducationalProgram entity) {
+        return new Program(
+                entity.getId(),
+                entity.getName(),
+                entity.getTrainingDirection(),
+                entity.getSemesterIdToRequiredCreditsCount()
+        );
+    }
+}
