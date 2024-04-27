fb2ee35bd27569d8fa1b138bd6cdd93dbf6f95b2
lev <lkorasik83@gmail.com>
2024-04-27 21:40:37 +0500

    Матрица

--- front/src/base_components/NewStudyPlan/NewStudyPlan.tsx
+++ front/src/base_components/NewStudyPlan/NewStudyPlan.tsx
@@ -4,50 +4,60 @@ import { useApis } from "../../apis/ApiBase/ApiProvider";
 import { ModuleDto } from "../../apis/api/modules/ModuleDto";
 
 export function NewStudyPlan() {
-    const [modules, setModules] = useState<ModuleDto[]>([])
+    const [matrix, setMatrix] = useState<boolean[][]>([])
 
     const api = useApis()
 
     useEffect(() => {
         const loadModules = async () => {
-            const modules = await api.educationalModulesApi.getAllModules();
-            setModules(modules)
+            const modules = await api.educationalModulesApi.getAllModules()
+
+            const newMatrix = []
+            for (let i = 0; i < modules.length; i++) {
+                newMatrix.push([false, false, false, false])
+            }
+            setMatrix(newMatrix)
+
+            console.log(newMatrix)
         }
         loadModules().catch(console.error)
-    })
+    }, [])
 
-    const renderModule = (module: ModuleDto) => {
-        return (
-            <tr className={styles.tr}>
-                <td className={styles.td}>{module.name}</td>
-                <td className={styles.td}>O</td>
-                <td className={styles.td}>O</td>
-                <td className={styles.td}>O</td>
-                <td className={styles.td}>O</td>
-            </tr>
-        )
+    const onClick = (x: number, y: number) => {
+        const newMatrix = []
+        for (let i = 0; i < matrix.length; i++) {
+            const newRow = []
+            for (let j = 0; j < matrix[i].length; j++) {
+                if ((i == y) && (j == x)) {
+                    newRow.push(!matrix[i][j])
+                } else {
+                    newRow.push(matrix[i][j])
+                }
+            }
+            newMatrix.push(newRow);
+        }
+        setMatrix(newMatrix)
+        console.log(newMatrix)
     }
 
-    const renderModules = () => {
-        return modules.map(module => renderModule(module))
-    } 
+    const renderText = (x: number, y: number) => {
+        return matrix[y][x] ? "T" : "F"
+    }
 
-    const renderHeader = () => {
-        return (
+    const renderRows = () => {
+        return matrix.map((row, index) => 
             <tr className={styles.tr}>
-                <td className={styles.td}>Название</td>
-                <td className={styles.td}>1</td>
-                <td className={styles.td}>2</td>
-                <td className={styles.td}>3</td>
-                <td className={styles.td}>4</td>
+                <td className={styles.td} onClick={(e) => onClick(0, index)}>{renderText(0, index)}</td>
+                <td className={styles.td} onClick={(e) => onClick(1, index)}>{renderText(1, index)}</td>
+                <td className={styles.td} onClick={(e) => onClick(2, index)}>{renderText(2, index)}</td>
+                <td className={styles.td} onClick={(e) => onClick(3, index)}>{renderText(3, index)}</td>
             </tr>
         )
     }
 
     return (
         <table className={styles.table}>
-            {renderHeader()}
-            {renderModules()}
+            {renderRows()}
         </table>
     )
 }
