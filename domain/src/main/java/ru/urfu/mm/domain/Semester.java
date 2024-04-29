da911e8cf025c85d61134a37a97e00100d0adf06
lev <lkorasik83@gmail.com>
2024-04-29 15:18:42 +0500

    Исправил семестр

--- domain/src/main/java/ru/urfu/mm/domain/Semester.java
+++ domain/src/main/java/ru/urfu/mm/domain/Semester.java
@@ -4,9 +4,21 @@ import ru.urfu.mm.domain.enums.SemesterType;
 
 import java.util.UUID;
 
+/**
+ * Семестр
+ */
 public class Semester {
+    /**
+     * Идентификатор семестра
+     */
     private UUID id;
+    /**
+     * Год, в котором проходит семестр
+     */
     private int year;
+    /**
+     * Тип семестра
+     */
     private SemesterType type;
 
     public Semester() {
