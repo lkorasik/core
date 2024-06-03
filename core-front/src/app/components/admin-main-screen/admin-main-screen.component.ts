ff4236f99e0b9ccfe140896c52e7584e43683ac9
Lev <lkorasik83@gmail.com>
2024-06-03 20:47:56 +0500

    Add container component

--- core-front/src/app/components/admin-main-screen/admin-main-screen.component.ts
+++ core-front/src/app/components/admin-main-screen/admin-main-screen.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ActionBarComponent } from '../action-bar/action-bar.component';
 import { EducationalProgramScreenComponent } from '../educational-program-screen/educational-program-screen.component';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from '../container/container.component';
 
 @Component({
   selector: 'app-admin-main-screen',
   standalone: true,
-  imports: [ActionBarComponent, RouterOutlet],
+  imports: [ActionBarComponent, RouterOutlet, ContainerComponent],
   templateUrl: './admin-main-screen.component.html',
   styleUrl: './admin-main-screen.component.css'
 })
