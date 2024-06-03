89ac77ee7623455910b23a02114f2eba89f9e4ff
Lev <lkorasik83@gmail.com>
2024-06-03 21:04:10 +0500

    Add title component

--- core-front/src/app/components/admin-main-screen/admin-main-screen.component.ts
+++ core-front/src/app/components/admin-main-screen/admin-main-screen.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { ActionBarComponent } from '../action-bar/action-bar.component';
-import { EducationalProgramScreenComponent } from '../educational-program-screen/educational-program-screen.component';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from '../container/container.component';
+import { TitleComponent } from '../title/title.component';
 
 @Component({
-  selector: 'app-admin-main-screen',
-  standalone: true,
-  imports: [ActionBarComponent, RouterOutlet, ContainerComponent],
-  templateUrl: './admin-main-screen.component.html',
-  styleUrl: './admin-main-screen.component.css'
+    selector: 'app-admin-main-screen',
+    standalone: true,
+    imports: [ActionBarComponent, RouterOutlet, ContainerComponent, TitleComponent],
+    templateUrl: './admin-main-screen.component.html',
+    styleUrl: './admin-main-screen.component.css'
 })
 export class AdminMainScreenComponent {
 }
