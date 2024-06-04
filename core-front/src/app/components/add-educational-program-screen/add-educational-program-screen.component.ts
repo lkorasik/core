ce67fe82f7f9e5a2bcd63d8aad3c376fd49dce90
Lev <lkorasik83@gmail.com>
2024-06-04 20:19:10 +0500

    Add toolbar

--- core-front/src/app/components/add-educational-program-screen/add-educational-program-screen.component.ts
+++ core-front/src/app/components/add-educational-program-screen/add-educational-program-screen.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from '../title/title.component';
 import { TextFieldComponent } from '../text-field/text-field.component';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
 
 @Component({
     selector: 'app-add-educational-program-screen',
     standalone: true,
-    imports: [TitleComponent, TextFieldComponent],
+    imports: [TitleComponent, TextFieldComponent, ToolbarComponent],
     templateUrl: './add-educational-program-screen.component.html',
     styleUrl: './add-educational-program-screen.component.css'
 })
---
+++ core-front/src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TitleComponent } from '../title/title.component';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [TitleComponent],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.css'
+})
+export class ToolbarComponent {
+
+}
